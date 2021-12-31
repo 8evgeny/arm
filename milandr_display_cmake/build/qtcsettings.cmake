0ad1d95c2a344a49554b705388e84883eae50f2b
8evgeny <8evgeny@gmail.com>
2021-12-31 15:20:37 +0300

    +

--- milandr_display_cmake/build/qtcsettings.cmake
+++ milandr_display_cmake/build/qtcsettings.cmake
@@ -1,2 +1,8 @@
 # This file is managed by Qt Creator, do not edit!
 
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/evg/Qt5/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/home/evg/toolchain/gcc-arm-none-eabi-10.3-2021.10/bin/arm-none-eabi-cpp" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/home/evg/toolchain/gcc-arm-none-eabi-10.3-2021.10/bin/arm-none-eabi-g++" CACHE "STRING" "" FORCE)
--- milandr_display_cmake/cmake-build-debug/CMakeFiles/display_Cmake.elf.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for display_Cmake.elf.
-# This may be replaced when dependencies are built.
