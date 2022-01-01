f3ef519c48b49eccbfda005054ab91521eda7187
8evgeny <8evgeny@gmail.com>
2022-01-01 17:19:35 +0300

    add

---
+++ Qbs3_QtCreator template for Milandr microcpu/app/src/main.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include "stdio.h"
+
+void Soft_Delay(volatile uint32_t number)
+{
+	while(number--);
+}
+
+int main(void)
+{
+
+	int a = 0;
+    while (1) {
+    a++;
+    Soft_Delay(10000);
+    }
+}
