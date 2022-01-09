bcd46a6ec38a3daf53d104d95e25c4fb3fe56b0d
8evgeny <8evgeny@gmail.com>
2022-01-09 08:38:51 +0300

    Timer begin

---
+++ MILANDR_EXAMPLES_1986BE91T/3_timer/src/main.cpp
@@ -0,0 +1,28 @@
+#include "MDR32Fx.h"
+#include "MDR32F9Qx_rst_clk.h"
+#include "MDR32F9Qx_port.h"
+#include "MDR32F9Qx_timer.h"
+#include "MDR32F9Qx_iwdg.h"
+//---------------------------------
+#include "MDR32F9Qx_config.h"
+#include "MDR32F9Qx_usb_handlers.h"
+//---------------------------------
+#include "main.h"
+//=================================
+
+
+
+int main (int argc, char** argv) {
+
+
+    while(1){
+
+    }
+}
+#if 0
+Описание структуры, инициализации таймера
+typedef struct
+{
+
+}TIMER_CntInitTypeDef;
+#endif
