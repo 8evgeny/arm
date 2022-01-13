d15730134f5b41c8880e51ad3fe9737b8eba9211
Evgeny Parubets <8evgeny@gmail.com>
2022-01-13 17:47:29 +0300

    +

--- milandr_display_cmake/src/main.cpp
+++ milandr_display_cmake/src/main.cpp
@@ -6,15 +6,12 @@
 //=================================
 
 int main (int argc, char** argv) {
+
 	__disable_irq();
-//	SysTick_init();
- //   delay_ms(2000);
 	RCC_init();
-//    std::string aa = "qqq";
-//    std::vector<int> vec{1,2,3,45};
-//    vec.push_back(55);
 	GPIO_init();
 	__enable_irq();
+
 	TIM1_init();
 	IWDT_init();
     VCom_Configuration();
