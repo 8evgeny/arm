dcd220baefb6f40cc44182a5c3ba485518ca76d8
Evgeny Parubets <8evgeny@gmail.com>
2022-03-17 11:05:37 +0300

    скорость 115200

--- STM32_PROJECTS/Beginning_STM32_book/rtos/uart/uart.c
+++ STM32_PROJECTS/Beginning_STM32_book/rtos/uart/uart.c
@@ -37,7 +37,8 @@ uart_setup(void) {
 		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
 		GPIO_USART1_TX);
 
-	usart_set_baudrate(USART1,38400);
+//	usart_set_baudrate(USART1,38400);
+    usart_set_baudrate(USART1, 115200);
 	usart_set_databits(USART1,8);
 	usart_set_stopbits(USART1,USART_STOPBITS_1);
 	usart_set_mode(USART1,USART_MODE_TX);
