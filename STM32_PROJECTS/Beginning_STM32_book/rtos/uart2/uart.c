26a5166725c947bd6bb7b0385572684a31d77948
8evgeny <8evgeny@gmail.com>
2022-04-02 11:35:12 +0300

    ++

--- STM32_PROJECTS/Beginning_STM32_book/rtos/uart2/uart.c
+++ STM32_PROJECTS/Beginning_STM32_book/rtos/uart2/uart.c
@@ -26,22 +26,22 @@ static QueueHandle_t uart_txq;		// TX queue for UART
 static void
 uart_setup(void) {
 
-    rcc_periph_clock_enable(RCC_GPIOC);
-    rcc_periph_clock_enable(RCC_UART4);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_USART2);
 
-    // UART TX on PC10 (GPIO_USART4_TX)
-    gpio_set_mode(GPIOC,
+    // UART TX on PA2 (GPIO_USART4_TX)
+    gpio_set_mode(GPIOA,
 		GPIO_MODE_OUTPUT_50_MHZ,
 		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-        GPIO_UART4_TX);
+        GPIO_USART2_TX);
 
-    usart_set_baudrate(UART4,115200);
-    usart_set_databits(UART4,8);
-    usart_set_stopbits(UART4,USART_STOPBITS_1);
-    usart_set_mode(UART4,USART_MODE_TX);
-    usart_set_parity(UART4,USART_PARITY_NONE);
-    usart_set_flow_control(UART4,USART_FLOWCONTROL_NONE);
-    usart_enable(UART4);
+    usart_set_baudrate(USART2,115200);
+    usart_set_databits(USART2,8);
+    usart_set_stopbits(USART2,USART_STOPBITS_1);
+    usart_set_mode(USART2,USART_MODE_TX);
+    usart_set_parity(USART2,USART_PARITY_NONE);
+    usart_set_flow_control(USART2,USART_FLOWCONTROL_NONE);
+    usart_enable(USART2);
 
 	// Create a queue for data to transmit from UART
 	uart_txq = xQueueCreate(256,sizeof(char));
@@ -57,9 +57,9 @@ uart_task(void *args __attribute__((unused))) {
 	for (;;) {
 		// Receive char to be TX
         if ( xQueueReceive(uart_txq,&ch,10) == pdPASS ) {
-            while ( !usart_get_flag(UART4, USART_SR_TXE) )
+            while ( !usart_get_flag(USART2, USART_SR_TXE) )
 				taskYIELD();	// Yield until ready
-            usart_send(UART4, ch);
+            usart_send(USART2, ch);
 		}
 		// Toggle LED to show signs of life
         gpio_toggle(GPIOD,GPIO2);
@@ -104,7 +104,7 @@ demo_task(void *args __attribute__((unused)))
 int
 main(void) {
 
-    rcc_clock_setup_in_hse_8mhz_out_72mhz();	// CPU clock is 72 MHz
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
 	// GPIO PC13:
     rcc_periph_clock_enable(RCC_GPIOD);
