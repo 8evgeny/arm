e5d089dd6a0f7ce03a333c5c4946024ddeb4aa67
Evgeny Parubets <8evgeny@gmail.com>
2023-07-13 15:34:23 +0300

    ++

--- STM32_PROJECTS/NucleoF103RB/RUST/src/main.rs
+++ STM32_PROJECTS/NucleoF103RB/RUST/src/main.rs
@@ -6,18 +6,13 @@ use panic_halt as _;
 
 use nb::block;
 
-use stm32f1xx_hal::{
-    prelude::*,
-    pac,
-    timer::Timer,
-};
 use cortex_m_rt::entry;
 use embedded_hal::digital::v2::OutputPin;
+use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 
 // Определяем входную функцию.
 #[entry]
 fn main() -> ! {
-
     // Получаем управление над аппаратными средствами
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
@@ -28,14 +23,13 @@ fn main() -> ! {
     // let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
     let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
 
-    // Конфигурируем пин b12 как двухтактный выход.
+    // Конфигурируем пин a5 как двухтактный выход.
     // Регистр "crh" передаётся в функцию для настройки порта.
     // Для пинов 0-7, необходимо передавать регистр "crl".
     // let mut led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
     let mut led1 = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
     // Конфигурируем системный таймер на запуск обновления каждую секунду.
-    let mut timer = Timer::syst(cp.SYST, &clocks)
-        .start_count_down(1.hz());
+    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(2.hz());
 
     // Ждём пока таймер запустит обновление
     // и изменит состояние светодиода.
