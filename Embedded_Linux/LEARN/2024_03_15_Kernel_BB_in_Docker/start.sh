0e0db9a0f322d856beecc95303b5c74919e96435
8evgeny <8evgeny@gmail.com>
2024-03-15 20:22:14 +0300

    stage Docker

---
+++ Embedded_Linux/LEARN/2024_03_15_Kernel_BB_in_Docker/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# docker build  --no-cache -t buildroot  /home/evg/SOFT/Github/Arm/Embedded_Linux/3_buildroot
+docker build -t buil_bb_kernel  /home/evg/SOFT/Github/Arm/Embedded_Linux/LEARN/2024_03_15_Kernel_BB_in_Docker
