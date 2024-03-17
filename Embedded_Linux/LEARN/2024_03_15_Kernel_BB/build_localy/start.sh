4ca36c3e4753a61ad962645e7e71069af1f651f1
8evgeny <8evgeny@gmail.com>
2024-03-17 06:33:22 +0300

    проверка директории

---
+++ Embedded_Linux/LEARN/2024_03_15_Kernel_BB/build_localy/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+cd /home/evg/SOFT/Github/Arm/Embedded_Linux/LEARN/2024_03_15_Kernel_BB/build_localy
+arm-linux-gnueabihf-gcc --version
+
+# если нет папки u-boot
+if [ ! -d u-boot ]; then
+    echo u-boot not exist
+    git clone --single-branch --branch u-boot-2016.09.y --progress git://git.denx.de/u-boot.git u-boot/
+    cd u-boot
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_boneblack_defconfig
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+else
+    echo u-boot exist
+fi
