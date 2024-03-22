2cd26856a666475719704b06f4be0744ca79567e
evgeny <8evgeny@gmail.com>
2024-03-22 22:17:12 +0300

    ++

--- Embedded_Linux/2024_03_22_Buildroot/build_in_Docker/start.sh
+++ Embedded_Linux/2024_03_22_Buildroot/build_in_Docker/start.sh
@@ -2,7 +2,7 @@
 #docker build --no-cache -t build_bb_kernel  ${PWD}
 docker build -t buildroot_bbb_kernel  ${PWD}
 docker run --name bbb_image buildroot_bbb_kernel
-mkdir -p images
+rm -rf images
 docker cp bbb_image:/home/evg/buildroot/output/images .
 docker rm bbb_image 
 
