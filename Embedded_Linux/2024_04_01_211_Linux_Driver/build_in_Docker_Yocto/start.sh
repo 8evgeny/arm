6b0dc28e11feec63fe2e3888846032bb29e0e5ab
evgeny <8evgeny@gmail.com>
2024-04-08 22:32:02 +0300

    yocto

--- Embedded_Linux/2024_04_01_211_Linux_Driver/build_in_Docker_Yocto/start.sh
+++ Embedded_Linux/2024_04_01_211_Linux_Driver/build_in_Docker_Yocto/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #docker build --no-cache -t build_bb_kernel  ${PWD}
-docker build -t yocto_rpi_kernel  ${PWD}
-docker run -it --name rpi_image yocto_rpi_kernel
+docker build -t yocto_rpi_image  ${PWD}
+docker run -it --name rpi_image yocto_rpi_image
 rm -rf images
 # docker cp rpi_image:/home/evg/buildroot/output/images .
 docker rm rpi_image 
