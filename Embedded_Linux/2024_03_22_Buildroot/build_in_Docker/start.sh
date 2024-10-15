cf988580a0e71dcba14cce9712e9593a556a2bc2
Evgeny <8evgeny@gmail.com>
2024-10-15 07:26:53 +0300

    +

--- Embedded_Linux/2024_03_22_Buildroot/build_in_Docker/start.sh
+++ Embedded_Linux/2024_03_22_Buildroot/build_in_Docker/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-#docker build --no-cache -t orange_pi_one_image  ${PWD}
-docker build -t buildroot_raspberry_pi4_image  ${PWD}
-docker run --name rpi4_image   buildroot_raspberry_pi4_image
+docker build --no-cache -t orange_pi_one_image  ${PWD}
+#docker build -t buildroot_raspberry_pi4_image  ${PWD}
+docker run --name orange_pi_one_image   orange_pi_one_image
 rm -rf images
-docker cp rpi4_image:/home/evg/buildroot/output/images .
-docker rm rpi4_image 
+docker cp orange_pi_one_image:/home/user/buildroot/output/images .
+docker rm orange_pi_one_image 
 
 
