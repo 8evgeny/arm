0b8470c48986fbe91d54ea349325fc52198eb997
Evgeny <8evgeny@gmail.com>
2024-10-20 19:48:49 +0300

    books

---
+++ Embedded_Linux/2024_03_22_Buildroot/build_in_Docker/start_2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+docker build --no-cache -t rock4_image  ${PWD}
+#docker run --name rock4_image   rock4_image
+#rm -rf images
+#docker cp rock4_image:/home/user/buildroot/output/images .
+#docker rm rock4_image 
+
+
