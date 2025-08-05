2e47c2515f97d172650d1daabdca69a538714031
Evgeny <8evgeny@gmail.com>
2025-08-05 10:01:59 +0300

    1

--- STM32_NEW_PROJECTS/STM32G030F6P6TR/Core/Src/main.c
+++ STM32_NEW_PROJECTS/STM32G030F6P6TR/Core/Src/main.c
@@ -135,13 +135,13 @@ int main(void)
 
   while (1)
   {
-    read_Temp();
-    read_U_I();
-    printf("get RD_VA1P8\r\n");
-    print_MP2790(RD_VA1P8);
-    printf("get RD_VA3P3\r\n");
-    print_MP2790(RD_VA3P3);
-    printf("\r\n");
+//    read_Temp();
+//    read_U_I();
+//    printf("get RD_VA1P8\r\n");
+//    print_MP2790(RD_VA1P8);
+//    printf("get RD_VA3P3\r\n");
+//    print_MP2790(RD_VA3P3);
+//    printf("\r\n");
 
     HAL_Delay(4000);
 
