22462ee16eca638e390043778c4c9caeb4b0b93b
Evgeny <8evgeny@gmail.com>
2025-08-06 15:25:44 +0300

    +

--- STM32_NEW_PROJECTS/STM32G030F6P6TR/Core/Src/main.c
+++ STM32_NEW_PROJECTS/STM32G030F6P6TR/Core/Src/main.c
@@ -132,11 +132,13 @@ int main(void)
 //    read_2790_REGS();
 //    read_42790_REGS();
 
-
+read_Temp();
+HAL_Delay(1000);
+read_Temp();
   while (1)
   {
-    read_Temp();
-    read_U_I();
+//    read_Temp();
+//    read_U_I();
 
     HAL_Delay(10000);
 
